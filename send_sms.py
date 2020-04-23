8e90fc18332652b116bc000c093bbffb365ade9d
Samantha Gatt <smfgatt@gmail.com>
2020-04-23 12:57:58 -0400

    Sends a text to my phone number through Twilio Messaging Service

---
+++ send_sms.py
@@ -0,0 +1,22 @@
+# Download the helper library from https://www.twilio.com/docs/python/install
+from twilio.rest import Client
+from dotenv import load_dotenv
+load_dotenv(verbose=True)
+import os
+
+# Your Account Sid and Auth Token from twilio.com/console
+TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
+TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+TWILIO_MESSAGING_SERVICE_SID = os.getenv("TWILIO_MESSAGING_SERVICE_SID")
+MY_PHONE_NUMBER = os.getenv("MY_PHONE_NUMBER")
+
+client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+
+message = client.messages \
+    .create(
+         body="Hello world\nFrom Sammie",
+         messaging_service_sid=TWILIO_MESSAGING_SERVICE_SID,
+         to=MY_PHONE_NUMBER
+     )
+
+print(message.sid)
